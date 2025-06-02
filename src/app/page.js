f004839d04b8a7389d51f1d9a45d254432b95107
Marcxzz <angionimarco2.0@gmail.com>
2025-06-02 15:27:42 +0200

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,21 @@
 export default function Home() {
+  const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+  
   return (
-    <main>
-      <div>Hello world!</div>
-    </main>
+    <>
+      {/* <div>Page top</div> */}
+      <div className="flex gap-4 overflow-x-auto">
+        {array.map((item, i) => {
+          return (
+            <div className="card" key={item}>
+              <p>{i}</p>
+            </div>
+          )
+        })}
+      </div>
+
+      <div style={{height: '1000px'}}></div>
+      {/* <div>Page bottom</div> */}
+    </>
   );
 }
---
+++ src/components/common/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+	return (
+		<div className="dark:bg-gray-900 w-full p-4 font-semibold text-2xl sticky top-0">
+			Wordiary
+		</div>
+	)
+}
