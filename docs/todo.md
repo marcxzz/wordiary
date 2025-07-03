6341dcb590bd329a9200020224f71c30493d8fc2
Marcxzz <angionimarco2.0@gmail.com>
2025-07-03 22:39:20 +0200

    minor UI changes

--- docs/todo.md
+++ docs/todo.md
@@ -2,18 +2,21 @@
 
 # Medium priority
 - import custom font
+- expand translation lenght to 100 (?)
 - add tags (verb, adjective, slang, proverb, techical term)
-- create dictionary page
 
 # Low priority
-- format correct date in word card
-- add sorting options
-- use search query as URL param
-- improve word details page
 - write README.md
+- improve word details page
+- format correct date in word card (wtf)
+- create dictionary page
 
 # Ideas
+- settings page (default from/to lang, current theme, app language (not now), etc.)
+- add sorting options at /words
+- use search query as URL param
 - different themes
+- favorites list (new db field)
+- button to hear pronunciation (text-to-speech)
 - integrate translation API
 - random new word suggestion
-- button to hear pronunciation (text-to-speech)
--- src/app/add/page.js
+++ src/app/add/page.js
@@ -127,16 +127,18 @@ export default function AddPage() {
       <section>
         <h1 className="h1">Add new word</h1>
 
-        <div className="flex p-1 rounded-2xl dark:bg-gray-700 border border-gray-600 overflow-hidden">
-          <div className="p-2">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5 text-gray-300">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" />
-            </svg>
+        <div className="flex rounded-2xl dark:bg-gray-700 border border-gray-600 overflow-hidden">
+          <div className="p-1 flex w-full">
+            <div className="p-2">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5 text-gray-300">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" />
+              </svg>
+            </div>
+            <input type="text" placeholder="Type the original word..." value={word || ''} id="wordInput" className="w-full outline-none dark:text-white text-sm" required maxLength={50} onChange={(event) => handleWordUpdate(event)} />
           </div>
-          <input type="text" placeholder="Type the original word..." value={word || ''} id="wordInput" className="w-full outline-none dark:text-white text-sm" required maxLength={50} onChange={(event) => handleWordUpdate(event)} />
 
-          <form className="max-w-sm mx-auto">
-            <select id="fromLangSelect" value={fromLang || 'default'} className="outline-none dark:text-white text-sm border border-gray-600 overflow-hidden rounded-xl w-auto h-full px-1 dark:bg-gray-700" onChange={(event) => handleFromLangChange(event)}>
+          <form className="ml-auto">
+            <select id="fromLangSelect" value={fromLang || 'default'} className="outline-none dark:text-white text-sm border-l border-gray-600 overflow-hidden w-auto h-full px-2 dark:bg-gray-700" onChange={(event) => handleFromLangChange(event)}>
               <option value='default' disabled>From</option>
               <option value="enUS">EN (US)</option>
               <option value="enUK">EN (UK)</option>
@@ -147,16 +149,18 @@ export default function AddPage() {
           </form>
         </div>
 
-        <div className="flex p-1 mt-2 rounded-2xl dark:bg-gray-700 border border-gray-600 overflow-hidden">
-          <div className="p-2">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5 text-gray-300">
-              <path strokeLinecap="round" strokeLinejoin="round" d="m10.5 21 5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 0 1 6-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 0 1-3.827-5.802" />
-            </svg>
+        <div className="flex rounded-2xl dark:bg-gray-700 border border-gray-600 overflow-hidden mt-2">
+          <div className="p-1 flex w-full">
+            <div className="p-2">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5 text-gray-300">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m10.5 21 5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 0 1 6-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 0 1-3.827-5.802" />
+              </svg>
+            </div>
+            <input type="text" placeholder="Type the translation..." value={translation || ''} id="translationInput" className="w-full outline-none dark:text-white text-sm" required maxLength={50} onChange={(event) => handleTranslationUpdate(event)} />
           </div>
-          <input type="text" placeholder="Type the translation..." value={translation || ''} id="translationInput" className="w-full outline-none dark:text-white text-sm" required maxLength={50} onChange={(event) => handleTranslationUpdate(event)} />
 
-          <form className="max-w-sm mx-auto">
-            <select id="toLangSelect" value={toLang || 'default'} className="outline-none dark:text-white text-sm border border-gray-600 overflow-hidden rounded-xl w-auto h-full px-1 dark:bg-gray-700" onChange={(event) => handleToLangChange(event)}>
+          <form className="ml-auto">
+            <select id="toLangSelect" value={toLang || 'default'} className="outline-none dark:text-white text-sm border-l border-gray-600 overflow-hidden w-auto h-full px-2 dark:bg-gray-700" onChange={(event) => handleToLangChange(event)}>
               <option value='default' disabled>To</option>
               <option value="enUS">EN (US)</option>
               <option value="enUK">EN (UK)</option>
@@ -184,7 +188,7 @@ export default function AddPage() {
 
       <section>
         <h1 className="h1 mb-2!">Import words</h1>
-        <p className="text-sm text-gray-500 mb-4">Paste words formatted as <em>word: translation</em></p>
+        <p className="text-sm text-gray-500 mb-8">Paste words formatted as <b><em>word: translation</em></b></p>
         
         <div className="flex items-center justify-between mb-2">
           <form className="max-w-sm">
@@ -197,7 +201,7 @@ export default function AddPage() {
               <option value="esES">ES</option>
             </select>
           </form>
-
+          <hr className="w-full mx-8 border-gray-600" />
           <form className="max-w-sm">
             <select id="importToLangSelect" value={importToLang || 'default'} className="outline-none dark:text-white text-sm border border-gray-600 overflow-hidden rounded-2xl w-auto h-full p-2 dark:bg-gray-700" onChange={(event) => handleImportToLangChange(event)}>
               <option value='default' disabled>To</option>
@@ -213,7 +217,7 @@ export default function AddPage() {
         <textarea id="importInput" value={importText || ''} className="bg-gray-700 border border-gray-600 rounded-2xl w-full p-2 px-3 text-sm outline-none min-h-[150px] max-h-[350px]" onChange={(event) => handleImportedTextTyping(event)} placeholder="Paste your words here..."></textarea>
 
         <button type="button" className="button button-primary w-full mt-[10px]" onClick={() => handleImportBtnClick()}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5 my-auto">
             <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
           </svg>
 
